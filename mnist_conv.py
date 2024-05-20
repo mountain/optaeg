25fb029bd5d15ddef7cccf76c6592fe50a9757be
Mingli Yuan <mingli.yuan@gmail.com>
2024-05-20 13:45:53 +0800

    add conv version mnist_conv.py

---
+++ mnist_conv.py
@@ -0,0 +1,367 @@
+# We give the conv varaints of OptAEG here.
+# the convolution part is credits to: https://github.com/detkov/Convolution-From-Scratch/ and https://github.com/AntonioTepsich/Convolutional-KANs
+
+import torch as torch
+import torch.nn.functional as F
+import lightning as ltn
+import argparse
+import lightning.pytorch as pl
+import numpy as np
+import math
+
+from typing import List, Tuple, Union
+from torch import Tensor
+from torch import nn
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--n_epochs", type=int, default=1000, help="number of epochs of training")
+parser.add_argument("-b", "--batch", type=int, default=256, help="batch size of training")
+parser.add_argument("-m", "--model", type=str, default='mnist0', help="model to execute")
+opt = parser.parse_args()
+
+if torch.cuda.is_available():
+    accelerator = 'gpu'
+    torch.set_float32_matmul_precision('medium')
+elif torch.backends.mps.is_available():
+    accelerator = 'cpu'
+else:
+    accelerator = 'cpu'
+
+
+def calc_out_dims(matrix, kernel_side, stride, dilation, padding):
+    batch_size,n_channels,n, m = matrix.shape
+    h_out = np.floor((n + 2 * padding[0] - kernel_side - (kernel_side - 1) * (dilation[0] - 1)) / stride[0]).astype(int) + 1
+    w_out = np.floor((m + 2 * padding[1] - kernel_side - (kernel_side - 1) * (dilation[1] - 1)) / stride[1]).astype(int) + 1
+    b = [kernel_side // 2, kernel_side// 2]
+    return h_out,w_out,batch_size,n_channels
+
+
+def conv2d(matrix: Union[List[List[float]], np.ndarray],
+             kernel, 
+             kernel_side,
+             stride= (1, 1), 
+             dilation= (1, 1), 
+             padding= (0, 0),
+             device= "cuda"
+             ) -> torch.Tensor:
+    h_out, w_out,batch_size,n_channels = calc_out_dims(matrix, kernel_side, stride, dilation, padding)
+    
+    matrix_out = torch.zeros((batch_size,n_channels,h_out,w_out)).to(device)
+    unfold = torch.nn.Unfold((kernel_side,kernel_side), dilation=dilation, padding=padding, stride=stride)
+
+
+    for channel in range(n_channels):
+        #print(matrix[:,channel,:,:].unsqueeze(1).shape)
+        conv_groups = unfold(matrix[:,channel,:,:].unsqueeze(1)).transpose(1, 2)
+        #print("conv",conv_groups.shape)
+        for k in range(batch_size):
+            matrix_out[k,channel,:,:] = kernel.forward(conv_groups[k,:,:]).reshape((h_out,w_out))
+    return matrix_out
+
+
+def multiple_convs_conv2d(matrix,
+             kernels, 
+             kernel_side,
+             stride= (1, 1), 
+             dilation= (1, 1), 
+             padding= (0, 0),
+             device= "cuda"
+             ) -> torch.Tensor:
+    h_out, w_out,batch_size,n_channels = calc_out_dims(matrix, kernel_side, stride, dilation, padding)
+    n_convs = len(kernels)
+    matrix_out = torch.zeros((batch_size,n_channels*n_convs,h_out,w_out)).to(device)
+    unfold = torch.nn.Unfold((kernel_side,kernel_side), dilation=dilation, padding=padding, stride=stride)
+    conv_groups = unfold(matrix[:,:,:,:]).view(batch_size, n_channels,  kernel_side*kernel_side, h_out*w_out).transpose(2, 3)#reshape((batch_size,n_channels,h_out,w_out))
+    for channel in range(n_channels):
+        for kern in range(n_convs):
+            matrix_out[:,kern  + channel*n_convs,:,:] = kernels[kern].conv.forward(conv_groups[:,channel,:,:].flatten(0,1)).reshape((batch_size,h_out,w_out))
+    return matrix_out
+
+
+def add_padding(matrix: np.ndarray, 
+                padding: Tuple[int, int]) -> np.ndarray:
+    n, m = matrix.shape
+    r, c = padding
+    
+    padded_matrix = np.zeros((n + r * 2, m + c * 2))
+    padded_matrix[r : n + r, c : m + c] = matrix
+    
+    return padded_matrix
+
+
+class Unit(torch.nn.Module):
+    def __init__(self, in_features, out_features, base_activation):
+        super(Unit, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.base_activation = base_activation()
+        self.base_weight = torch.nn.Parameter(torch.Tensor(out_features, in_features))
+        torch.nn.init.kaiming_uniform_(self.base_weight, a=math.sqrt(5))
+ 
+    def forward(self, x: torch.Tensor):
+        assert x.dim() == 2 and x.size(1) == self.in_features
+
+        base_output = F.linear(self.base_activation(x), self.base_weight)
+        return base_output
+
+
+class Conv2d(torch.nn.Module):
+    def __init__(
+            self,
+            n_convs: int = 1,
+            kernel_size: tuple = (2,2),
+            stride: tuple = (1,1),
+            padding: tuple = (0,0),
+            dilation: tuple = (1,1),
+            base_activation = None,
+            device: str = "cpu"
+        ):
+        super(Conv2d, self).__init__()
+        self.kernel_size = kernel_size
+        self.device = device
+        self.dilation = dilation
+        self.padding = padding
+        self.convs = torch.nn.ModuleList()
+        self.n_convs = n_convs
+        self.stride = stride
+        self.base_activation = base_activation
+
+        # Create n_convs Convolution objects
+        for _ in range(n_convs):
+            self.convs.append(
+                Convolution(
+                    kernel_size= kernel_size,
+                    stride = stride,
+                    padding=padding,
+                    dilation = dilation,
+                    base_activation = base_activation,
+                    device = device
+                )
+            )
+
+    def forward(self, x: torch.Tensor, update_grid=False):
+        # If there are multiple convolutions, apply them all
+        if self.n_convs > 1:
+            return multiple_convs_conv2d(x, self.convs,self.kernel_size[0],self.stride,self.dilation,self.padding,self.device)
+
+        # If there is only one convolution, apply it
+        return self.convs[0].forward(x)
+        
+
+class Convolution(torch.nn.Module):
+    def __init__(
+            self,
+            kernel_size: tuple = (2,2),
+            stride: tuple = (1,1),
+            padding: tuple = (0,0),
+            dilation: tuple = (1,1),
+            grid_size: int = 5,
+            base_activation = None,
+            device = "cpu"
+        ):
+        """
+        Args
+        """
+        super(Convolution, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.device = device
+        self.conv = Unit(
+            in_features = math.prod(kernel_size),
+            out_features = 1,
+            base_activation = base_activation,
+        )
+
+    def forward(self, x: torch.Tensor, update_grid=False):
+        return conv2d(x, self.conv, self.kernel_size[0], self.stride, self.dilation, self.padding, self.device)
+
+    def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
+        return sum(layer.regularization_loss(regularize_activation, regularize_entropy) for layer in self.layers)
+
+
+class OptAEGV3(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vx = nn.Parameter(torch.zeros(1, 1, 1))
+        self.vy = nn.Parameter(torch.ones(1, 1, 1))
+        self.wx = nn.Parameter(torch.zeros(1, 1, 1))
+        self.wy = nn.Parameter(torch.ones(1, 1, 1))
+        self.afactor = nn.Parameter(torch.zeros(1, 1))
+        self.mfactor = nn.Parameter(torch.ones(1, 1))
+
+    # @torch.compile
+    def flow(self, dx, dy, data):
+        return data * (1 + dy) + dx
+
+    # @torch.compile
+    def forward(self, data):
+        shape = data.size()
+        data = data.flatten(1)
+        data = data - data.mean()
+        data = data / data.std()
+
+        b = shape[0]
+        v = self.flow(self.vx, self.vy, data.view(b, -1, 1))
+        w = self.flow(self.wx, self.wy, data.view(b, -1, 1))
+
+        dx = self.afactor * torch.sum(v * torch.sigmoid(w), dim=-1)
+        dy = self.mfactor * torch.tanh(data)
+        data = self.flow(dx, dy, data)
+
+        return data.view(*shape)
+
+
+class MNISTModel(ltn.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.learning_rate = 1e-3
+        self.counter = 0
+        self.labeled_loss = 0
+        self.labeled_correct = 0
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 53)
+        return [optimizer], [scheduler]
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        x = x.view(-1, 1, 28, 28)
+        z = self.forward(x)
+        loss = F.nll_loss(z, y)
+
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        x = x.view(-1, 1, 28, 28)
+
+        z = self.forward(x)
+        loss = F.nll_loss(z, y)
+        self.log('val_loss', loss, prog_bar=True)
+
+        pred = z.data.max(1, keepdim=True)[1]
+        correct = pred.eq(y.data.view_as(pred)).sum() / y.size()[0]
+        self.log('correct_rate', correct, prog_bar=True)
+
+        self.labeled_loss += loss.item() * y.size()[0]
+        self.labeled_correct += correct.item() * y.size()[0]
+        self.counter += y.size()[0]
+
+    def test_step(self, test_batch, batch_idx):
+        x, y = test_batch
+        x = x.view(-1, 1, 28, 28)
+        z = self(x)
+
+        pred = z.data.max(1, keepdim=True)[1]
+        correct = pred.eq(y.data.view_as(pred)).sum() / y.size()[0]
+        self.log('correct_rate', correct, prog_bar=True)
+
+    def on_save_checkpoint(self, checkpoint) -> None:
+        import glob, os
+
+        correct = self.labeled_correct / self.counter
+        loss = self.labeled_loss / self.counter
+        record = '%2.5f-%03d-%1.5f.ckpt' % (correct, checkpoint['epoch'], loss)
+        fname = 'best-%s' % record
+        with open(fname, 'bw') as f:
+            torch.save(checkpoint, f)
+        for ix, ckpt in enumerate(sorted(glob.glob('best-*.ckpt'), reverse=True)):
+            if ix > 5:
+                os.unlink(ckpt)
+
+        self.counter = 0
+        self.labeled_loss = 0
+        self.labeled_correct = 0
+
+        print()
+
+
+class MNIST_OptAEGV3(MNISTModel):
+    def __init__(self):
+        super().__init__()
+        self.pool = nn.MaxPool2d(2)
+        self.conv0 = Conv2d(n_convs=4, kernel_size=(3,3), padding=(1,1), base_activation=OptAEGV3)
+        self.conv1 = Conv2d(n_convs=1, kernel_size=(3,3), padding=(1,1), base_activation=OptAEGV3)
+        self.conv2 = Conv2d(n_convs=1, kernel_size=(3,3), padding=(1,1), base_activation=OptAEGV3)
+        self.fc = nn.Linear(4 * 3 * 3, 10, bias=False)
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = self.pool(x)
+        x = self.conv1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+
+def test_best():
+    import glob
+    fname = sorted(glob.glob('best-*.ckpt'), reverse=True)[0]
+    with open(fname, 'rb') as f:
+        model = MNIST_OptAEGV3()
+        checkpoint = torch.load(f)
+        model.load_state_dict(checkpoint['state_dict'], strict=False)
+        model = model.cpu()
+        model.eval()
+
+        print('')
+        with torch.no_grad():
+            counter, success = 0, 0
+            for test_batch in test_loader:
+                x, y = test_batch
+                x, y = x.cpu(), y.cpu()
+                x = x.view(-1, 1, 28, 28)
+                z = model(x)
+                pred = z.data.max(1, keepdim=True)[1]
+                correct = pred.eq(y.data.view_as(pred)).sum() / y.size()[0]
+                print('.', end='', flush=True)
+                success += correct.item()
+                counter += 1
+                if counter % 100 == 0:
+                    print('')
+        print('')
+        print('Accuracy: %2.5f' % (success / counter))
+        torch.save(model, 'mnist-optaeg-v3.pt')
+
+
+if __name__ == '__main__':
+    print('loading data...')
+    from torch.utils.data import DataLoader
+    from torchvision.datasets import MNIST
+    from torchvision import transforms
+
+    mnist_train = MNIST('datasets', train=True, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+    ]))
+
+    mnist_test = MNIST('datasets', train=False, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+    ]))
+
+    train_loader = DataLoader(mnist_train, shuffle=True, batch_size=opt.batch, num_workers=8, persistent_workers=True)
+    val_loader = DataLoader(mnist_test, batch_size=opt.batch, num_workers=8, persistent_workers=True)
+    test_loader = DataLoader(mnist_test, batch_size=opt.batch, num_workers=8, persistent_workers=True)
+
+    # training
+    print('construct trainer...')
+    trainer = pl.Trainer(accelerator=accelerator, precision=32, max_epochs=opt.n_epochs,
+                         callbacks=[EarlyStopping(monitor="val_loss", mode="min", patience=30)])
+
+    print('construct model...')
+    model = MNIST_OptAEGV3()
+
+    print('training...')
+    trainer.fit(model, train_loader, val_loader)
+
+    print('testing...')
+    test_best()
