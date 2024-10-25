29a975f286aca35c57390de87e2daac22ac948a4
Mingli Yuan <mingli.yuan@gmail.com>
2024-10-26 03:14:39 +0800

    add a naive cnn version

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -186,11 +186,10 @@ class MNISTModel(ltn.LightningModule):
 class MNIST_CNN(MNISTModel):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 5, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(5, 5, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(2)
-        self.dropout = nn.Dropout(0.25)
-        self.fc = nn.Linear(5 * 3 * 3, 10)
+        self.fc = nn.Linear(128 * 3 * 3, 10)
         self.act01 = OptAEGV3()
         self.act02 = OptAEGV3()
         self.act03 = OptAEGV3()
