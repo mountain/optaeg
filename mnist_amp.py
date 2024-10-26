38061ad70b0c20a6090ded8f2c05511fbd1f2d74
Mingli Yuan <mingli.yuan@gmail.com>
2024-10-26 11:09:20 +0800

    add a naive cnn version

--- mnist_amp.py
+++ mnist_amp.py
@@ -19,7 +19,7 @@ if th.cuda.is_available():
     accelerator = 'gpu'
     th.set_float32_matmul_precision('medium')
 elif th.backends.mps.is_available():
-    accelerator = 'cpu'
+    accelerator = 'mps'
 else:
     accelerator = 'cpu'
 
